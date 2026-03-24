c42d834a4f9656bc3edb204d3027df3dd12f7d8c
Vedanti Koparkar <koparkarvedanti21@gmail.com>
2026-03-24 23:54:19 +0530

    fixed DB connection public network

--- src/main/java/com/safevault/util/DBConnection.java
+++ src/main/java/com/safevault/util/DBConnection.java
@@ -14,15 +14,14 @@ public class DBConnection {
             String url = "jdbc:mysql://centerbeam.proxy.rlwy.net:49268/railway"
                     + "?useSSL=false"
                     + "&allowPublicKeyRetrieval=true"
-                    + "&serverTimezone=UTC"
-                    + "&autoReconnect=true";
+                    + "&serverTimezone=UTC";
 
             String user = "root";
             String password = "xMdSIvopVWdGukNXDQtrfVTaIHBQqfqd";
 
             con = DriverManager.getConnection(url, user, password);
 
-            System.out.println("✅ Database Connected Successfully");
+            System.out.println("✅ DB Connected");
 
         } catch (Exception e) {
             e.printStackTrace();
