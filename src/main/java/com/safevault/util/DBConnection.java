cc74ec8a04269a5cde1fcb9588b7c17aaf6a03c7
Vedanti Koparkar <koparkarvedanti21@gmail.com>
2026-03-24 23:00:54 +0530

    final db connected

--- src/main/java/com/safevault/util/DBConnection.java
+++ src/main/java/com/safevault/util/DBConnection.java
@@ -6,22 +6,18 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     public static Connection getConnection() {
-
         Connection con = null;
 
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/safevault_db",
-                    "root",
-                    "Admin123");
-
-            System.out.println("Database Connected Successfully");
+                "jdbc:mysql://centerbeam.proxy.rlwy.net:49268/railway",
+                "root",
+                "xMdSIvopVWdGukNXDQtrfVTaIHBQqfqd"
+            );
 
         } catch (Exception e) {
-
             e.printStackTrace();
         }
 
